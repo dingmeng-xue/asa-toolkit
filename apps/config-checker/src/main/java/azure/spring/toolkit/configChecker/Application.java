dabe815b4e982e9fa473ef0e3bcabe4f6f9eeef4
Dingmeng Xue <dixue@microsoft.com>
2023-07-14 14:54:23 +0800

    add project

---
+++ apps/config-checker/src/main/java/azure/spring/toolkit/configChecker/Application.java
@@ -0,0 +1,28 @@
+package azure.spring.toolkit.configChecker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@Configuration
+@EnableAutoConfiguration
+public class Application {
+
+	@Value("${config.name}")
+	String name = "World";
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello " + name;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
